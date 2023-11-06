63055055e2ac63841c24cad0be4b4f662511e794
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2023-11-05 22:32:47 -0300

    Controller do QRcode

--- src/app/controller/QrcodeController.js
+++ src/app/controller/QrcodeController.js
@@ -1,21 +1,43 @@
 import qrcode from 'qrcode'
 class QrcodeController {
 
+
+  create(req, res){
+    const nome_req = req.body.dados.nome_evento;
+    const id_req = req.body.dados.id_evento;
+    const url= `http://localhost:3000/inicio/Cadastrar_usuario`
+    const combinedData = `${url}?id:${id_req}`;
+    qrcode.toDataURL(combinedData, (err, qr) => {
+      if (err) {
+        res.status(500).json({ error: 'Erro na geração do QR Code' });
+      } else {
+        res.send(qr);
+        console.log("Imagem Gerada")
+      }
+
+      // qrcode.toFile('qrcode.png', combinedData, (err) => {
+      //   if (err) {
+      //     console.error(err);
+      //   } else {
+      //     console.log('QR code salvo como qrcode.png');
+      //   }
+      // });
+    });
+  }
+
+
   store(req, res) {
     // console.log(req.body)
     const nome_req = req.body.dados.nome_evento;
     const id_req = req.body.dados.id_evento;
 
-
-    qrcode.toDataURL('https://www.google.com/', (err, url) => {
+    qrcode.toFile('qrcode.png', combinedData, (err) => {
       if (err) {
-        res.status(500).json({ error: 'Erro na geração do QR Code' });
+        console.error(err);
       } else {
-        res.send(url);
+        console.log('QR code salvo como qrcode.png');
       }
     });
-
-    console.log("Imagem Gerada")
 
   }
 
