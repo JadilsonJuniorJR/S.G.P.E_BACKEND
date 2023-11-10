bf7ab2b84482d8798a218afa40ce622ea4b56a3b
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2023-11-09 21:38:09 -0300

    Criando TryCatch para lidar com erros

--- src/app/controller/ParticipantesController.js
+++ src/app/controller/ParticipantesController.js
@@ -24,26 +24,31 @@ class ParticipanteController {
         // console.log(req.body)
         const nome_req = req.body.dados.nome_user;
         const matricula_req = req.body.dados.matricula;
+        const id_evento_req = (req.body.dados.id_evento);
         const curso_req = req.body.dados.curso;
         const campus_req = req.body.dados.campus;
         const email_req = req.body.dados.email;
-        const dataHoraRequisicao = new Date().toLocaleString('pt-BR', {day: '2-digit',month: 'numeric',year: 'numeric',hour: '2-digit',minute: '2-digit'})
+        const dataHoraRequisicao = new Date().toLocaleString('pt-BR', { day: '2-digit', month: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit' })
 
-        // console.log(dataHoraRequisicao)
-        const resultado_criacao = await ParticipanteRepository.create(nome_req, matricula_req, curso_req, campus_req, email_req, dataHoraRequisicao)
-        res.json(resultado_criacao)
+        try {
+            const resultado_criacao = await ParticipanteRepository.create(nome_req, matricula_req, curso_req, campus_req, email_req, dataHoraRequisicao, id_evento_req)
+            res.json(resultado_criacao)
+        } catch (error) {
+            res.status(400)
+            console.log("ERRO")
+        }
 
     }
 
     // Atualizar Dado do Usuario 
     async update(req, res) {
-        const dataHoraRequisicao = new Date().toLocaleString('pt-BR', {day: '2-digit',month: 'numeric',year: 'numeric',hour: '2-digit',minute: '2-digit'})
-        const nome_req = req.params.nome_user.toLowerCase();
+        const dataHoraRequisicao = new Date().toLocaleString('pt-BR', { day: '2-digit', month: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit' })
+        const nome_req = req.params.nome;
         const matricula_req = req.params.matricula;
 
         const resposta_consulta = await ParticipanteRepository.findById(nome_req, matricula_req)
         // console.log(resposta_consulta.rowCount)
-        
+
         if (resposta_consulta.rowCount == 0) {
             res.status(404).json({ Erro: 'Participante não registrado no sistema!' })
         } else {
