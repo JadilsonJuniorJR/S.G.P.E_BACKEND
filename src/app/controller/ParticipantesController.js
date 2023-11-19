3c47e9e55a311a4811d2690b21a7ccc41e79a19c
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2023-11-19 00:26:42 -0300

    Alteração no metodo de Atualização
    foi adicionado ao método uma logica que realiza a verificação da presença e inicializa o campo participação, necessário para o método de verificação.

--- src/app/controller/ParticipantesController.js
+++ src/app/controller/ParticipantesController.js
@@ -1,4 +1,7 @@
+// import moment from "moment";
+import moment from "moment-timezone";
 import ParticipanteRepository from "../repositories/ParticipanteRepository.js";
+import VerificarPresenca from "../services/VerificarPresenca.js";
 // Classe Responsavel por Chama os Metodos do CRUD 
 // Responsavel pelas respostas para os usuarios 
 class ParticipanteController {
@@ -42,18 +45,38 @@ class ParticipanteController {
 
     // Atualizar Dado do Usuario 
     async update(req, res) {
-        const dataHoraRequisicao = new Date().toLocaleString('pt-BR', { day: '2-digit', month: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit' })
+
+        let dataHoraRequisicao = moment().tz('America/Sao_Paulo').format('DD-MM-YYYY HH:mm')
+        // const dataHoraRequisicao = new Date().toLocaleString('pt-BR', { day: '2-digit', month: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit' })
         const nome_req = req.params.nome;
         const matricula_req = req.params.matricula;
+        console.log(dataHoraRequisicao)
+        let resposta_consulta = await ParticipanteRepository.findById(nome_req, matricula_req)
+        let validar_participacao = false
+        // console.log("Resposta Consulta")
+        console.log(resposta_consulta)
+        console.log(resposta_consulta[0].participacao)
+        // console.log("Resposta Consulta Linhas")
+        // console.log(resposta_consulta.length)
 
-        const resposta_consulta = await ParticipanteRepository.findById(nome_req, matricula_req)
+
         // console.log(resposta_consulta.rowCount)
-
-        if (resposta_consulta.rowCount == 0) {
+        // Verifica se na pesquisa Realizada, se Há algum retorno, indicando que a um usuaria cadastrado.
+        if (resposta_consulta.length == 0) {
             res.status(404).json({ Erro: 'Participante não registrado no sistema!' })
-        } else {
-            const resultado_alteracao = await ParticipanteRepository.update(nome_req, matricula_req, dataHoraRequisicao)
-            res.json(resultado_alteracao)
+
+            // VERIFICA SE ALGUM CAMPO DE PARTICIPAÇÃO JÁ FOI UTILIZADO CASO SIM O USUARIO JÁ REALIZOU O CADASTRO
+        } else if (resposta_consulta[0].participacao == true || resposta_consulta[0].participacao == false) {
+            console.log("USUARIO JÁ CADASTRADO")
+            res.status(409).send("USUARIO JÁ CADASTRADO")
+        }else {
+            // CASO O USUARIO AINDA NÃO CONFIRMOU A PRESENÇA É REALIZADO UMA ATUALIZAÇÃO NA DATA, HORA DE SAIDA E CAMPO PARTICIPAÇÃO
+            // TAMBÉM É VERIFICADO A VALIDAÇÃO DE PRESENCIA
+            validar_participacao = false
+            await ParticipanteRepository.update(nome_req, matricula_req, dataHoraRequisicao, validar_participacao)
+            let nova_resposta_consulta = await ParticipanteRepository.findById(nome_req, matricula_req)
+            await VerificarPresenca.verificar(nova_resposta_consulta)
+            res.status(200).send("USUARIO CADASTRADO NO SISTEMA")
         }
     }
 
