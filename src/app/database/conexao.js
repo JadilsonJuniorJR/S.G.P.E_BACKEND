52f521ea21bf8c3a57aeb9e0b1c54f382af61aa9
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2023-12-04 18:21:21 -0300

    Teste Conexão

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -2,15 +2,23 @@
 import pg from 'pg';
 
 // Conexão Do Banco 
+// const conexao = new pg.Client(
+//     {
+//         host: 'localhost',
+//         port: 5432,
+//         user: 'postgres',
+//         password: 'admin',
+//         database: 'SGPE'
+//     }
+// )
+
+//Conexão Do Banco 
 const conexao = new pg.Client(
     {
-        host: 'localhost',
-        port: 5432,
-        user: 'postgres',
-        password: 'admin',
-        database: 'SGPE'
+        connectionString: process.env.POSTGRES_URL + "?sslmode=require",
     }
 )
+
 conexao.connect()
 
 export const consulta = (sql, valores= ' ', mensagemReject) => {
